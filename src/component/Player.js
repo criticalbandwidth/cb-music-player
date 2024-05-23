fb5fd492423e2955af319a84891c89aa3ecf87f2
Andreas <andreas@criticalbandwidth.com>
2024-05-23 07:16:00 -0400

    Added timeline to music player

--- src/component/Player.js
+++ src/component/Player.js
@@ -1,15 +1,29 @@
 import { useEffect, useState } from "react"; 
 import useSound from "use-sound"; // for handling the sound
-import qala from "../assets/LoveMeOrLeaveMe.wav"; // importing the music
+import song from "../assets/LoveMeOrLeaveMe.wav"; // importing the music
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 import { IconContext } from "react-icons"; // for customazing the icons
 
 export default function Player() {
 
+  //Variable Declarations
   const [isPlaying, setIsPlaying] = useState(false);
-  const [play, { pause, duration, sound }] = useSound(qala);
+  const [play, { pause, duration, sound }] = useSound(song);
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: "",
+  }); // current position of the audio in minutes and seconds
+  const [seconds, setSeconds] = useState(); // current position of the audio in seconds
+  const sec = duration / 1000;
+  const min = Math.floor(sec / 60);
+  const secRemain = Math.floor(sec % 60);
+  const time = {
+    min: min,
+    sec: secRemain
+  };
   
+
   const playingButton = () => {
     if (isPlaying) {
       pause(); // this will pause the audio
@@ -19,13 +33,29 @@ export default function Player() {
       setIsPlaying(true);
     }
   };
-  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([])); // setting the seconds state with the current state
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec,
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
   return (
   <div className="component">
     <h2>Music Player</h2>
     <img
       className="musicCover"
       src="https://picsum.photos/200/200"
+      alt="Music Player Cover"
     />
     <div>
       <h3 className="title">Andreas Blachere</h3>
@@ -56,6 +86,27 @@ export default function Player() {
         </IconContext.Provider>
       </button>
     </div>
+    <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
   </div>
 ) ;
 }
